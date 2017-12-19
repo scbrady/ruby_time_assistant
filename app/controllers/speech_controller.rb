f4db012e80a7ee3e33133ab752d181b4bf71a6e2
Scott Brady <scott@prolio.com>
2017-12-18 19:04:10 -0500

    Assistant stuff

--- app/controllers/speech_controller.rb
+++ app/controllers/speech_controller.rb
@@ -2,4 +2,24 @@ class SpeechController < ApplicationController
   def index
     json_response(message: 'authed')
   end
+
+  def conversation
+    assistant_response = GoogleAssistant.respond_to(params, response) do |assistant|
+      assistant.intent.main do
+        assistant.ask(
+            prompt: "<speak>Hi there! Say something, please.</speak>",
+            no_input_prompt: [
+                "<speak>If you said something, I didn't hear you.</speak>",
+                "<speak>Did you say something?</speak>"
+            ]
+        )
+      end
+
+      assistant.intent.text do
+        assistant.tell("<speak>I can respond, too!</speak>")
+      end
+    end
+
+    json_response assistant_response
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'auth/failure', to: 'sessions#failure'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  post '/speech' => 'speech#conversation'
+
   root 'speech#index'
 end
