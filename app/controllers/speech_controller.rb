74589b8763018d246c878d2bfa24dc6c6587ed83
Scott Brady <scott@prolio.com>
2017-12-21 10:52:44 -0500

    Getting it just to work

--- app/controllers/speech_controller.rb
+++ app/controllers/speech_controller.rb
@@ -5,19 +5,21 @@ class SpeechController < ApplicationController
   end
 
   def conversation
-    assistant_response = GoogleAssistant.respond_to(params, response) do |assistant|
+    params2 = params["originalRequest"]["data"]
+
+    assistant_response = GoogleAssistant.respond_to(params2, response) do |assistant|
       assistant.intent.main do
         assistant.ask(
-            "<speak>Hi there! Say something, please.</speak>",            # The voice prompt the user will hear.
+            "<speak>Hi there! Say something, please.</speak>",
             [
-                "<speak>If you said something, I didn't hear you.</speak>", # You can provide a number of "no input prompts". A random
-                "<speak>Did you say something?</speak>"                     # one will be spoken if the user takes too long to respond.
+                "<speak>If you said something, I didn't hear you.</speak>",
+                "<speak>Did you say something?</speak>"
             ]
         )
       end
 
       assistant.intent.text do
-        assistant.tell("<speak>Thanks for talking! Goodbye!</speak>")   # Both SSML and plain text work here and anywhere you send a prompt.
+        assistant.tell("<speak>I can respond, too!</speak>")
       end
     end
 
