7b003ac2df5a35bfc9aa3c84d8d9786148657a0f
Scott Brady <scott@prolio.com>
2017-12-21 09:31:49 -0500

    Had to change it here too

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :authorize_request, only: [:create, :destroy, :failure]
+  # skip_before_action :authorize_request, only: [:create, :destroy, :failure]
 
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
