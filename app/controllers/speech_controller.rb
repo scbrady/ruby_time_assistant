1482ec69db3ed4e0e88b5e2640a2c1b8c07673c2
Scott Brady <scott@prolio.com>
2017-12-21 10:21:51 -0500

    Trying to upgrade to v2 of the assistant API

--- app/controllers/speech_controller.rb
+++ app/controllers/speech_controller.rb
@@ -1,4 +1,5 @@
 class SpeechController < ApplicationController
+
   def index
     json_response(message: 'authed')
   end
@@ -7,16 +8,16 @@ class SpeechController < ApplicationController
     assistant_response = GoogleAssistant.respond_to(params, response) do |assistant|
       assistant.intent.main do
         assistant.ask(
-            prompt: "<speak>Hi there! Say something, please.</speak>",
-            no_input_prompt: [
-                "<speak>If you said something, I didn't hear you.</speak>",
-                "<speak>Did you say something?</speak>"
+            "<speak>Hi there! Say something, please.</speak>",            # The voice prompt the user will hear.
+            [
+                "<speak>If you said something, I didn't hear you.</speak>", # You can provide a number of "no input prompts". A random
+                "<speak>Did you say something?</speak>"                     # one will be spoken if the user takes too long to respond.
             ]
         )
       end
 
       assistant.intent.text do
-        assistant.tell("<speak>I can respond, too!</speak>")
+        assistant.tell("<speak>Thanks for talking! Goodbye!</speak>")   # Both SSML and plain text work here and anywhere you send a prompt.
       end
     end
 
